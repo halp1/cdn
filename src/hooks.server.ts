1909d34eb8c05bbc83c003a937f52660c84c8b0a
halp <haelpf@gmail.com>
2026-04-06 11:47:37 -0400

    cors something cors

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,9 +3,24 @@ import { statements } from "$lib/db";
 import { handleWebDAV } from "$lib/webdav/handler";
 import type { Handle } from "@sveltejs/kit";
 
+const CORS_PATHS = ["/api", "/obj"];
+
+const addCors = (response: Response) => {
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "*");
+  response.headers.set("Access-Control-Allow-Headers", "*");
+  return response;
+};
+
 export const handle: Handle = async ({ event, resolve }) => {
   if (event.url.pathname.startsWith("/webdav")) {
     return handleWebDAV(event.request, event.url);
+  }
+
+  const isCors = CORS_PATHS.some((p) => event.url.pathname.startsWith(p));
+
+  if (isCors && event.request.method === "OPTIONS") {
+    return addCors(new Response(null, { status: 204 }));
   }
 
   event.locals.user = null;
@@ -23,5 +38,7 @@ export const handle: Handle = async ({ event, resolve }) => {
       event.cookies.delete("token", { path: "/" });
     }
   }
-  return resolve(event);
+  const response = await resolve(event);
+  if (isCors) addCors(response);
+  return response;
 };
